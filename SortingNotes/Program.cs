473a1cce4922a084964ba091a05a047639d8b240
Yuliya-Babuk <Babukjulia1!@gmail.com>
2021-02-20 23:42:12 +0300

    init: Add solution and 2 projects (TypeConversion and SortingNotes)

---
+++ SortingNotes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SortingNotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Enter quantity of notes:\t");
+
+            int quantity = int.Parse(Console.ReadLine());
+            
+            int[] cash = new int[quantity];
+
+            Console.WriteLine("Enter notes");
+
+            for (int i = 0; i < quantity; i++)
+            {
+               
+                cash[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < quantity; i++)
+            {
+                for (int j = i+1; j <= (quantity - 1); j++)
+                {
+                    if (cash[i] > cash[j])
+                    {
+                        int temp = cash[i];
+                        cash[i] = cash[j];
+                        cash[j] = temp;
+                    }
+
+                }
+            }
+
+            Console.WriteLine("Notes after sorting:");
+
+            for (int i=0; i < quantity; i++)
+            {
+                Console.Write(cash[i] + "\t");
+               
+            }
+       
+        }
+    }
+}
---
+++ TypeConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte byteNumber = 255;
+
+            short shortNumber = byteNumber;
+            int intNumber = shortNumber;
+            float floatNumber = intNumber;
+
+            floatNumber = -1.5f;
+
+            intNumber = (int)floatNumber;
+
+            uint uintNumber = (uint)floatNumber;
+
+            uintNumber = 55111u;
+
+            byteNumber = (byte) uintNumber;
+
+            
+        }
+    }
+}
