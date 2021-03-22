ba1bf1852bb5c3be7e42571c8872b2c7e25bd68d
Yuliya-Babuk <Babukjulia1!@gmail.com>
2021-03-23 02:28:20 +0300

    init: Add project ExamTask and Class examples with Stack

--- ClassWork(StackExample)/Program.cs
+++ ClassWork(StackExample)/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using ClassWorkStackExample;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace ClassWork(StackExample)
+namespace ClassWorkStackExample
 {
     public enum StackOperation
     {
         Push,
         Pop
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public Group Group { get; set; }
+        public void TransferToAnotherGroup(Group gr)
+        {
+            Group = gr;
+        }
+
+    }
+    class Group
+    {
+        public string Number { get; set; }
     }
 
     class Program
@@ -42,6 +61,45 @@ namespace ClassWork(StackExample)
 
                 }
             }
+
+
+
+            Student s = new Student();
+
+            Group gr1 = new Group() { Number = "g1" };
+
+            Student st1 = new Student { Surname = "Ivanov", Name = "Oleg", Group = gr1 };
+            Student st2 = new Student { Name = "s2", Group = gr1 };
+
+            Group gr2 = new Group() { Number = "g2" };
+
+            Student st3 = new Student { Name = "s3", Group = gr2 };
+            Student st4 = new Student { Name = "s4", Group = gr2 };
+
+
+            List<Student> students = new List<Student> { st1, st2, st3, st4 };
+
+            List<string> initials = students.Select(student => student.Surname + " " + student.Name[0].ToString()).ToList();
+
+            st1.TransferToAnotherGroup(gr2);
+
+            List<Student> studentsGr1 = GetStudentsByGroup(students, gr1);
+            List<Student> studentsGr2 = GetStudentsByGroup(students, gr2);
+
+            static List<Student> GetStudentsByGroup(List<Student> allStudents, Group group)
+            {
+                List<Student> studentsByGroup = new List<Student>();
+
+                foreach (var student in allStudents)
+                {
+                    if (student.Group == group)
+                        studentsByGroup.Add(student);
+                }
+
+                return studentsByGroup;
+            }
+
         }
     }
 }
+
---
+++ ExamTask/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ExamTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] mas = { { {1,2 },{3,4 } },
+                {{4,5},{6,7} },
+                {{7,8},{9,10} },
+                {{10,11},{12,13} } };
+            Console.Write("{");
+            for (int i = 0; i <= mas.GetUpperBound(0); i++)
+            {
+                Console.Write("{");
+                for (int j = 0; j <= mas.GetUpperBound(1); j++)
+                {
+                    Console.Write("{");
+                    for (int k = 0; k <= mas.GetUpperBound(2); k++)
+                    {
+                        Console.Write(mas[i, j, k]);
+                        if (k != mas.GetUpperBound(2))
+                            Console.Write(",");
+
+                    }
+                    Console.Write("}");
+                    if (j != mas.GetUpperBound(1))
+                        Console.Write(",");
+                }
+                Console.Write("}");
+                if (i != mas.GetUpperBound(0))
+                    Console.Write(",");
+            }
+            Console.Write("}");
+
+            Console.WriteLine("\nEnter a string:");
+            string input = Console.ReadLine();
+            string output = "";
+
+            for (int i = 0; i < input.Length; i++)
+
+            {
+                if (i % 2 == 0)
+                {
+                    output = output + input[i];
+                }
+
+            }
+            Console.WriteLine(output);
+
+            List<int> listInput = new List<int>();
+            Random rn = new Random();
+            for (int i = 0; i < rn.Next(7, 9); i++)
+            {
+                listInput.Add(rn.Next(50));
+                Console.Write(listInput[i] + "\t");
+            }
+            List<int> maxNumber = listInput.Where(n => n % 2 == 0).OrderByDescending(n => n).Take(3).ToList();
+            Console.Write("\n3 max even numbers from the list:\n");
+            foreach (int number in maxNumber)
+            {
+                Console.Write(number + "\t");
+            }
+        }
+    }
+}
